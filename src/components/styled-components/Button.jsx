ea2ac8239a010f1b0e8427af6081ee787199b81d
Alan Tong <soup@Alans-MacBook-Pro.local>
2019-09-11 15:30:33 +0100

    separated button styled component

---
+++ src/components/styled-components/Button.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  margin-bottom: 10px;
+  font-size: 0.7rem;
+  border-radius: 5px;
+  padding: 5px 8px;
+  &:hover {
+    background: #e60059;
+  }
+`;
+
+export default Button;
