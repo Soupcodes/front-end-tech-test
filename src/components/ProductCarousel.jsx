e4f7c7ba5dd0121b1a21414b53fe656bd603a22a
Alan Tong <soup@Alans-MacBook-Pro.local>
2019-09-11 15:31:02 +0100

    separated image carousel styled component

--- src/components/ProductCarousel.jsx
+++ src/components/ProductCarousel.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import products from "../assets/ProductsList";
 import { Gallery, GalleryImage } from "react-gesture-gallery";
+import ImgCarousel from "../components/styled-components/CarouselSyledComponents";
 
 const ProductCarousel = () => {
   const [index, setIndex] = useState(0);
 
   return (
-    <div style={{ background: "white", width: "40vw", height: "40vh" }}>
+    <ImgCarousel>
       <Gallery
         index={index}
         onRequestChange={i => {
@@ -14,10 +15,19 @@ const ProductCarousel = () => {
         }}
       >
         {products[0].images.map(image => (
-          <GalleryImage objectFit="contain" src={image} key={image} />
+          <GalleryImage
+            objectFit="contain"
+            src={image}
+            key={image}
+            style={{
+              width: "40%",
+              height: "40%",
+              margin: "auto"
+            }}
+          />
         ))}
       </Gallery>
-    </div>
+    </ImgCarousel>
   );
 };
 
---
+++ src/components/styled-components/CarouselSyledComponents.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const ImgCarousel = styled.div`
+background: white, 
+width: 20%, 
+height: 20%
+`;
+
+export default ImgCarousel;
